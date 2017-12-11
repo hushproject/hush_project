a4f96e78903d6d6a891f21ad0135aae40f519650
Vladimir Gutorov <vlgutv@gmail.com>
2017-12-11 21:05:20 +0200

    Add base menu fixed position

--- themes/hush/src/js/modules/site_header/config.js
+++ themes/hush/src/js/modules/site_header/config.js
@@ -1,15 +1,29 @@
 {
+  function pinSiteLogo(logo) {
+    jQuery(window).scroll(() => {
+      let scrollPosition = jQuery(document).scrollTop();
+      if (scrollPosition > logo.offset().top) {
+        logo.addClass('fixedTop');
+      } else {
+        logo.removeClass('fixedTop');
+      }
+    });
+  }
+
   function showHideSidebar() {
     jQuery('.siteHeader__burgerMenu__button').click(function() {
       let elements = jQuery('.siteSidebarMenu, .siteHeader__burgerMenu__button');
-      if(jQuery(this).hasClass('active')) {
+      if (jQuery(this).hasClass('active')) {
         elements.removeClass('active');
-      }else {
+      } else {
         elements.addClass('active');
       }
     });
   }
-  if(jQuery('.siteSidebarMenu')) {
+  if (jQuery('.siteSidebarMenu')) {
     showHideSidebar();
+  }
+  if (jQuery('.homeHeader__description .siteLogo')) {
+    pinSiteLogo(jQuery('.homeHeader__description .siteLogo'));
   }
 }
