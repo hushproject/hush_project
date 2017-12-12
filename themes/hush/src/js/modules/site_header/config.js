d1e1861d9dc7324356cfcb12612706f162565736
Vladimir Gutorov <vlgutv@gmail.com>
2017-12-12 20:29:28 +0200

    Add observer for burger color on different sections

--- themes/hush/src/js/modules/site_header/config.js
+++ themes/hush/src/js/modules/site_header/config.js
@@ -1,16 +1,31 @@
-import ScrollMagic from "scrollmagic";
+// import ScrollMagic from "scrollmagic";
 
 {
   function pinSiteLogo(logo) {
     let controller = new ScrollMagic.Controller(),
+      controller2 = new ScrollMagic.Controller(),
       scene = new ScrollMagic.Scene({
         triggerElement: ".homeHeader__description",
         offset: jQuery(window).height() / 2 - 200
+      }),
+      trigeredClasses = ".homeHeader__description .siteLogo, .siteHeader__burgerMenu__button";
+      jQuery('.hideLogoOnThisSection').each(function() {
+        let el = `#${jQuery(this).attr('id')}`;
+        new ScrollMagic.Scene({
+          triggerElement: el,
+          duration: jQuery(this).height()
+        }).setClassToggle(trigeredClasses,"noLogo").addTo(controller);
       });
-      scene.setClassToggle(".homeHeader__description .siteLogo, .siteHeader__burgerMenu__button","fixedTop");
+      jQuery('.darkMenu').each(function() {
+        let el = `#${jQuery(this).attr('id')}`;
+        new ScrollMagic.Scene({
+          triggerElement: el,
+          duration: jQuery(this).height()
+        }).addIndicators().setClassToggle(trigeredClasses,"darkThere").addTo(controller);
+      });
+      scene.setClassToggle(trigeredClasses,"fixedTop");
       scene.addTo(controller);
   }
-
   function showHideSidebar() {
     jQuery('.siteHeader__burgerMenu__button').click(function() {
       let elements = jQuery('.siteSidebarMenu, .siteHeader__burgerMenu__button');
