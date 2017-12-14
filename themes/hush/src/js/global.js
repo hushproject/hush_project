6ce2c52f7bd2fa53bc0a0d77ee9bb16470717c6b
Vladimir Gutorov <vlgutv@gmail.com>
2017-12-14 14:22:11 +0200

    Add request form

--- themes/hush/src/js/global.js
+++ themes/hush/src/js/global.js
@@ -1,3 +1,17 @@
 import "../sass/global.sass";
 import "./modules/site_header/config.js";
 import "./modules/home_page/config.js"
+jQuery('.sendMail').click(function() {
+  console.log('ss');
+  let send = jQuery.ajax({
+    url: "https://formspree.io/vlgutv@gmail.com",
+    method: "POST",
+    data: {
+      message: "hello!"
+    },
+    dataType: "json"
+  });
+  send.then((result) => {
+    console.log(result);
+  });
+});
