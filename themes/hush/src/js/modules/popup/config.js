f74933e0350df9d7fe72aed7ec37f51eb5c126b8
Vladimir Gutorov <vlgutv@gmail.com>
2017-12-26 15:04:49 +0200

    Add ajax for from sending

--- themes/hush/src/js/modules/popup/config.js
+++ themes/hush/src/js/modules/popup/config.js
@@ -20,32 +20,30 @@
     });
   };
   showHideForm();
-  jQuery('#sendRequest').submit(function(event) {
+  jQuery('#subForm').submit(function(event) {
     event.preventDefault();
     jQuery('.sendRequest__wrapper').fadeOut(300);
     jQuery('.formSending').fadeIn(300);
-    let formValues = {
-      name: jQuery('input[name=name]').val(),
-      whenAreYouThink: jQuery('select[name=whenareyouthinkingstring]').val(),
-      howManyPeople: jQuery('select[name=howmanypeoplestring]').val(),
-      email: jQuery('input[name=email]').val(),
-      sendBrochure: jQuery('input[name=pleasesendmeabrochurestring]').val(),
-      message: jQuery('textarea[name=message]').val(),
-      addToMailList: jQuery('input[name=pleaseaddmetoyourmailingliststring]').val()
-    },
-    send = jQuery.ajax({
-      url: "https://formspree.io/vlgutv@gmail.com",
-      method: "POST",
-      data: formValues,
-      dataType: "json"
-    });
-    send.then((result) => {
-      jQuery('.formSending').hide();
-      jQuery('.sitePopup__content__thankU').addClass('active');
-    });
+    $.getJSON(
+      this.action + "?callback=?",
+      $(this).serialize(),
+      function(data) {
+        if (data.Status === 400) {
+          alert("Error: " + data.Message);
+        } else {
+          jQuery('.formSending').hide();
+          jQuery('.sitePopup__content__thankU').addClass('active');
+        }
+      });
   });
   jQuery('.closeAfterSending').click(() => {
     jQuery('.sitePopup__content__thankU').removeClass('active');
     jQuery('.sendRequest__wrapper').show();
   });
 }
+$(function() {
+  $('#subForm').submit(function(e) {
+    e.preventDefault();
+
+  });
+});
