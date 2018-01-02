a2ca86186974c225a5c8030d11f8367885672195
Vladimir Gutorov <vlgutv@gmail.com>
2018-01-02 13:37:00 +0200

    Add location testing

--- themes/hush/src/js/modules/popup/config.js
+++ themes/hush/src/js/modules/popup/config.js
@@ -20,30 +20,43 @@
     });
   };
   showHideForm();
-  jQuery('#subForm').submit(function(event) {
-    event.preventDefault();
+  jQuery('#subForm').submit(function(e) {
+    var getPosition = function() {
+      return new Promise(function(resolve, reject) {
+        return navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+    };
+    let _this = this;
+    e.preventDefault();
+    if (jQuery('#getuserlocation').length) {
+      getPosition().then((position) => {
+        jQuery('#getuserlocation').val(`latitude: ${position.coords.latitude}
+            | longitude: ${position.coords.longitude}`);
+        sendForm(_this);
+      }).catch((err) => {
+        sendForm(_this);
+      });
+    }
+  });
+
+  function sendForm(_this) {
     jQuery('.sendRequest__wrapper').fadeOut(300);
     jQuery('.formSending').fadeIn(300);
     $.getJSON(
-      this.action + "?callback=?",
-      $(this).serialize(),
+      _this.action + "?callback=?",
+      $(_this).serialize(),
       function(data) {
         if (data.Status === 400) {
           alert("Error: " + data.Message);
         } else {
+          console.log(data);
           jQuery('.formSending').hide();
           jQuery('.sitePopup__content__thankU').addClass('active');
         }
       });
-  });
+  }
   jQuery('.closeAfterSending').click(() => {
     jQuery('.sitePopup__content__thankU').removeClass('active');
     jQuery('.sendRequest__wrapper').show();
   });
 }
-$(function() {
-  $('#subForm').submit(function(e) {
-    e.preventDefault();
-
-  });
-});
