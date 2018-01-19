239bc0cc90612f3f7cc40ecf420d6024f02ec33d
Vladimir Gutorov <vlgutv@gmail.com>
2018-01-19 12:30:36 +0200

    Update redirects

--- dato-config/redirects.js
+++ dato-config/redirects.js
@@ -1,7 +1,7 @@
 module.exports = {
   create: function(dato, root, i18n, locale) {
     var fs = require('fs');
-    fs.appendFile('static/_redirects', dato.redirects.map(i => `${i.item}\n`).join(""), function(err) {
+    fs.appendFile('static/_redirects', `${dato.redirect.rules}`, function(err) {
       console.log(err);
     });
   }
