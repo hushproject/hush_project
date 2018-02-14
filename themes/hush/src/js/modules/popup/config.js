bc93fb14e128ead80b6504ceca26b4ef89389453
Vladimir Gutorov <vlgutv@gmail.com>
2018-02-14 11:17:28 +0200

    Upgrade validation

--- themes/hush/src/js/modules/popup/config.js
+++ themes/hush/src/js/modules/popup/config.js
@@ -1,5 +1,4 @@
-import validate from "jquery-validation";
-{
+import validate from "jquery-validation"; {
   function showHideForm(scrollPosition) {
     if (!scrollPosition) {
       scrollPosition = jQuery(window).scrollTop();
@@ -16,63 +15,67 @@ import validate from "jquery-validation";
       e.preventDefault();
     });
     jQuery('.closePopup').click(() => {
-      if(jQuery('.siteSidebarMenu').hasClass('active')) {
+      if (jQuery('.siteSidebarMenu').hasClass('active')) {
         jQuery('.sitePopup').removeClass('active');
-      }else {
+      } else {
         jQuery("body").removeClass('noScroll');
         $("html, body").animate({
           scrollTop: scrollPosition
-        }, 0, function () {
+        }, 0, function() {
           jQuery('.sitePopup').removeClass('active');
-            scrollPosition = 0;
+          scrollPosition = 0;
         });
       }
     });
   };
   showHideForm();
-  $('#subForm').validate({
-    rules: {
-      "cm-name": {
-        required: true,
-        minlength: 2
-      },
-      "cm-fvktd-fvktd": {
-        required: true,
-        email: true
-      },
-      "cm-fo-vjldkt": {
-        required: true
-      }
-    },
-    success: function() {
-      let val1 = jQuery('input[name=cm-name]').val(),
-        val2 = jQuery('input[name=cm-fvktd-fvktd]').val(),
-        val3 = jQuery('select[name=cm-fo-vjldkt]').val();
-      if(val1.length > 2 && val2.length > 2 && val3 !== null) {
-        jQuery('#subForm').submit(function(e) {
-          var getPosition = function() {
-            return new Promise(function(resolve, reject) {
-              return navigator.geolocation.getCurrentPosition(resolve, reject);
-            });
-          };
-          let _this = this;
-          e.preventDefault();
-          jQuery('.sendRequest__wrapper').fadeOut(300);
-          jQuery('.formSending').fadeIn(300);
-          if (jQuery('#getuserlocation').length) {
-            getPosition().then((position) => {
-              jQuery('#getuserlocation').val(`${position.coords.latitude},${position.coords.longitude}`);
-              sendForm(_this)
-            }).catch((err) => {
-              sendForm(_this)
-            });
-          } else {
+  jQuery('#subForm').submit(function(e) {
+    e.preventDefault();
+    let val1 = jQuery('input[name=cm-name]').val(),
+      val2 = jQuery('input[name=cm-fvktd-fvktd]').val(),
+      val3 = jQuery('select[name=cm-fo-vjldkt]').val(),
+      email = /\S+@\S+\.\S+/.test(val2);
+    if (val1.length > 2 && email === true && val3 !== null) {
+      jQuery('#subForm').submit(function(e) {
+        var getPosition = function() {
+          return new Promise(function(resolve, reject) {
+            return navigator.geolocation.getCurrentPosition(resolve, reject);
+          });
+        };
+        let _this = this;
+        e.preventDefault();
+        jQuery('.sendRequest__wrapper').fadeOut(300);
+        jQuery('.formSending').fadeIn(300);
+        if (jQuery('#getuserlocation').length) {
+          getPosition().then((position) => {
+            jQuery('#getuserlocation').val(`${position.coords.latitude},${position.coords.longitude}`);
             sendForm(_this)
-          }
-        });
-      }
+          }).catch((err) => {
+            sendForm(_this)
+          });
+        } else {
+          sendForm(_this)
+        }
+      });
+    }
+    if (val1.length < 2) {
+      jQuery('input[name=cm-name]').addClass('error');
+    }else {
+      jQuery('input[name=cm-name]').removeClass('error');
+    }
+    if (!email) {
+      jQuery('input[name=cm-fvktd-fvktd]').addClass('error');
+    }else {
+      jQuery('input[name=cm-fvktd-fvktd]').removeClass('error');
+    }
+    if(val3 === null) {
+      jQuery('select[name=cm-fo-vjldkt]').addClass('error');
+    }else {
+      jQuery('select[name=cm-fo-vjldkt]').removeClass('error');
     }
   });
+
+
   function sendForm(_this) {
     $.getJSON(
       _this.action + "?callback=?",
@@ -107,10 +110,10 @@ import validate from "jquery-validation";
     let valueYes = jQuery(this).attr('data-yes'),
       valueNo = jQuery(this).attr('data-no'),
       valueCurrent = jQuery(this).val();
-      if(valueCurrent == valueYes) {
-        jQuery(this).val(valueNo);
-      }else {
-        jQuery(this).val(valueYes);
-      }
+    if (valueCurrent == valueYes) {
+      jQuery(this).val(valueNo);
+    } else {
+      jQuery(this).val(valueYes);
+    }
   });
 }
