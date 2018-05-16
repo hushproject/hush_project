d092ff09209e18e3ec959f392ff6e2337665de1b
Korobeynik <korobeynik77@gmail.com>
2018-05-16 12:33:45 +0300

    Add fixes for slider

--- themes/hush/src/js/modules/testimonials_slider/config.js
+++ themes/hush/src/js/modules/testimonials_slider/config.js
@@ -1,31 +1,30 @@
-import slick from "slick-carousel";
-{
-  if($('#testimonialSlider').length) {
-    let speed = $('#testimonialSlider').attr('data-speed');
-    if(speed.length > 0) {
-      $('#testimonialSlider').slick({
-        slidesToShow: 1,
-        arrows: true,
-        dots: false,
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: speed,
-        fade: true,
-        adaptiveHeight: false,
-        responsive: [
-            {
-              breakpoint: 640,
-              settings: {
+import slick from "slick-carousel"; {
+    if ($('#testimonialSlider').length) {
+        let speed = $('#testimonialSlider').attr('data-speed');
+        if (speed.length > 0) {
+            $('#testimonialSlider').slick({
                 slidesToShow: 1,
-                adaptiveHeight: true,
-                slidesToScroll: 1,
-                autoplay: false,
-                fade: false,
-                adaptiveHeight: true
-              }
-            }
-          ]
-      });
+                arrows: true,
+                dots: true,
+                speed: 600,
+                autoplay: true,
+                autoplaySpeed: speed,
+                //fade: true,
+                adaptiveHeight: false,
+                centerMode: true,
+                centerPadding: '60px',
+                responsive: [{
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 1,
+                        adaptiveHeight: true,
+                        slidesToScroll: 1,
+                        autoplay: false,
+                        fade: false,
+                        adaptiveHeight: true
+                    }
+                }]
+            });
+        }
     }
-  }
 }
